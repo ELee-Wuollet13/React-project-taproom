80405127151d304bfee9dd057785955a793b3fe3
Student <example@example.com>
2020-02-21 11:37:40 -0800

    add state and forms

--- src/App.js
+++ src/App.js
@@ -10,24 +10,84 @@ import { Switch, Route, Router } from 'react-router-dom';
 
 
 
-function App() {
-  return (
-    <div className="App">
-    <Navbar/>
-    <div className="App-header">
-    <Header/>
-    <Switch>
-    <div className="content">
-    <Route exact path="/" component={Home}/>
-    <Route exact path="/PintList" component={PintList}/>
-    <Route exact path="/PintListEmp" component={PintListEmp}/>
-    <Route exact path="/newKeg" component={newKeg}/>
+class App extends React.Component {
 
-    </div>
-    </Switch>
-    </div>
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterTicketList: []
+    };
+
+    this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
+  }
+  handleAddingNewTicketToList(newTicket){
+    console.log('newTicket: ', newTicket);
+    // const position = this.indexOf()
+    const newMasterTicketList = this.state.masterTicketList.slice();
+    // console.log('masterTicketList: ', this.state.masterTicketList);
+    // console.log('newMasterTicketList: ', newMasterTicketList);
+    newTicket.formattedWaitTime = (newTicket.timeOpen).fromNow(true)
+    newMasterTicketList.push(newTicket);
+    // console.log('newMasterTicketList: ', newMasterTicketList);
+    this.setState({
+      masterTicketList: newMasterTicketList
+    });
+  }
+
+
+  // handleAddingEditTicketToList =(editTicket) =>{
+    //
+    //   console.log(this.getIndex);
+    //   const editMasterTicketList = this.state.masterTicketList.slice(1);
+    //   console.log('masterTicketList: ', this.state.masterTicketList);
+    //   console.log('editMasterTicketList: ', editMasterTicketList);
+    //   editTicket.formattedWaitTime = (editTicket.timeOpen).fromNow(true)
+    //   editMasterTicketList.push(editTicket);
+    //   console.log('editMasterTicketList: ', editMasterTicketList);
+    //   this.setState({
+      //     masterTicketList: editMasterTicketList
+      //   });
+      // }
+
+      updateTicketElapsedWaitTime() {
+        console.log("check");
+        let newMasterTicketList = this.state.masterTicketList.slice();
+        newMasterTicketList.forEach((ticket) =>
+        ticket.formattedWaitTime = (ticket.timeOpen).fromNow(true)
+      );
+      this.setState({masterTicketList: newMasterTicketList})
+    }
+
+    componentDidMount() {
+      this.waitTimeUpdateTimer = setInterval(() =>
+      this.updateTicketElapsedWaitTime(),
+      5000
+    );
+  }
+  componentWillUnmount(){
+    clearInterval(this.waitTimeUpdateTimer);
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+      <Navbar/>
+      <div className="App-header">
+      <Header/>
+      <Switch>
+      <div className="content">
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/PintList" component={PintList}/>
+      <Route exact path="/PintListEmp" component={PintListEmp}/>
+      <Route exact path="/newKeg" component={newKeg}/>
+
+      </div>
+      </Switch>
+      </div>
+      </div>
+    );
+  }
 }
 
 
---
+++ src/newKegForm.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { v4 } from 'uuid';
+import Moment from 'moment';
+
+function NewKegForm(props){
+  let _names = null;
+  let _location = null;
+  let _issue = null;
+
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+  props.onNewKegCreation({names: _names.value, location: _location.value, issue: _issue.value, id: v4, timeOpen: new Moment()});
+    _names.value = '';
+    _location.value = '';
+    _issue.value = '';
+  }
+
+  return (
+    <div>
+      <form style={{margin: "10px", position: "relative", zIndex: "550"}}onSubmit={handleNewKegFormSubmission}>
+        <input
+        type='text'
+        id='names'
+        placeholder='Pair Names'
+        ref={(input) => {_names = input;}}/>
+        <input
+        type='text'
+        id='location'
+        placeholder='Location'
+        ref={(input) => {_location = input;}}/>
+        <textarea
+        id='issue'
+        placeholder='Describe your issue.'
+        ref={(textarea) => {_issue = textarea;}}/>
+        <button type='submit'>Help!</button>
+
+      </form>
+    </div>
+
+  );
+}
+NewKegForm.propTypes = {
+  onNewKegCreation: PropTypes.func
+};
+export default NewKegForm;
